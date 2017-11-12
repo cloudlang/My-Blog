64e260b89d48a62f4f6c017c59aa656d3c8a1460
cloudlang <30916952+cloudlang@users.noreply.github.com>
2017-11-12 20:32:45 +0800

    Update AuthController.java

--- src/main/java/com/my/blog/website/controller/admin/AuthController.java
+++ src/main/java/com/my/blog/website/controller/admin/AuthController.java
@@ -10,7 +10,7 @@ import com.my.blog.website.service.ILogService;
 import com.my.blog.website.service.IUserService;
 import com.my.blog.website.utils.Commons;
 import com.my.blog.website.utils.TaleUtils;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
